e8df56bbe3b5c051bd6d5815d8770f0f8a6d67e3
davidobi <dbiggestdave@yahoo.com>
2022-05-01 21:48:04 +0000

    link to source code

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -10,7 +10,7 @@ const Download = () => {
                 <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
                 <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on github</p>
             </div>
-            <button className={styles.btnPrimary}>Source Code</button>
+            <button className={styles.btnPrimary}> <a href="https://github.com/davidobi/simple-react-native-app" target={`_blank`}>Source Code</a> </button>
             <div className={styles.flexCenter}>
                 <img src={assets.scene} alt="download_png" className={styles.img} />
             </div>
