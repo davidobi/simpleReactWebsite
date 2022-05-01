89d09b40e6e9839c707620b9ec425c19bbca2a43
davidobi <dbiggestdave@yahoo.com>
2022-05-01 21:15:17 +0000

    download page setup and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from "./components";
-import assets from './assets'
+import assets from './assets';
+import styles from "./styles/Global";
 
 const App = () => {
   return (
@@ -34,7 +35,11 @@ const App = () => {
         mockupImg={assets.mockup}
         banner='banner02'
       />
+      <Download />
 
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {' '} <span className="bold">David Obi</span> </p>
+      </div>
     </>
   );
 }
