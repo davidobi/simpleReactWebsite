2f9137d4f58a78c250bbe4f2ef5139c2747cc99c
davidobi <dbiggestdave@yahoo.com>
2022-05-01 21:36:40 +0000

    update package.json

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({assetUrl, link}) => {
     >
         <img src={assetUrl} alt='expo_icon' className={`${styles.btnIcon}`} />
         <div className={`flex flex-col justify-start ml-4`}>
-            <p className={`${styles.btnText} font-normal text-xs`}>View it on</p>
+            <p className={`${styles.btnText} font-normal text-xs`}>Not available on</p>
             <p className={`${styles.btnText} font-bold text-sm`}>Expo Store</p>
             
         </div>
